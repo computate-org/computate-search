788d6cb0ea675dd1f115ff10013c4f4a7f0b73db
Christopher Tate <computate@computate.org>
2022-02-02 21:15:43 -0700

    Release version 4.2.4.0

--- bin/touch.sh
+++ bin/touch.sh
@@ -1,5 +1,4 @@
 touch ~/.local/src/computate-search/src/main/java/org/computate/search/wrap/Wrap.java
-touch ~/.local/src/computate-search/src/main/java/org/computate/search/request/SiteRequest.java
 touch ~/.local/src/computate-search/src/main/java/org/computate/search/tool/SearchTool.java
 touch ~/.local/src/computate-search/src/main/java/org/computate/search/request/SearchRequest.java
 touch ~/.local/src/computate-search/src/main/java/org/computate/search/response/solr/SolrResponse.java
