7dad2b85620e62f1ee7a7ac4a4fca463bad91f08
Christopher Tate <computate@computate.org>
2022-05-21 00:15:38 -0600

    Adding no wrap elements.

--- src/main/java/org/computate/search/tool/XmlTool.java
+++ src/main/java/org/computate/search/tool/XmlTool.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class XmlTool {
 
 	public static List<String> HTML_ELEMENTS_CLOSED = Arrays.asList("area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr");
-	public static List<String> HTML_ELEMENTS_NO_WRAP = Arrays.asList("script", "span", "a", "b", "i", "u", "title", "use", "h1", "h2", "h3", "h4", "h5", "h6");
+	public static List<String> HTML_ELEMENTS_NO_WRAP = Arrays.asList("script", "span", "a", "b", "i", "u", "title", "use", "h1", "h2", "h3", "h4", "h5", "h6", "p", "dt", "dd" );
 	
 	public static String escapeShQuotXml(String str) {
 		String result = str;
