d6dda9de16bf6af9ad3a9bf36f2d8089c38c5ad9
Christopher Tate <computate@computate.org>
2024-12-28 08:56:59 -0700

    Fixing the vars for OpenShift AI

--- bin/indexnow.sh
+++ bin/indexnow.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd $HOME/.local/src/computate.org
+cd $HOME/.local/src/computate-search
 env WATCH=false \
   GENERATE_NOW=true \
-  VARS_PATH=$HOME/.local/src/computate.org/vars.yaml \
+  VARS_PATH=$HOME/.local/src/computate-search/vars.yaml \
   COMPUTATE_SRC=$HOME/.local/src/computate \
   COMPUTATE_VERTX_SRC=$HOME/.local/src/computate-vertx \
   SITE_LANG=enUS \
